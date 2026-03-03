cd63ddb935f198a80104953acea75cff5234c7f9
vinuvi-work <vinuvi@dynamic-labs.ai>
2026-03-03 09:49:10 +0530

    add a delete action for each row

--- hospital-app/app/Filament/Resources/Appoinments/Tables/AppoinmentsTable.php
+++ hospital-app/app/Filament/Resources/Appoinments/Tables/AppoinmentsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Appoinments\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -53,6 +54,7 @@ class AppoinmentsTable
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
